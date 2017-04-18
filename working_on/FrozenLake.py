b406dcc85051c8a777e64a002b6a6fefede44c08
Chia-Pin Tseng <chiapintseng@gmail.com>
2017-04-17 19:19:28 -0700

    Optimize the calc flow

--- working_on/FrozenLake.py
+++ working_on/FrozenLake.py
@@ -3,7 +3,6 @@ import gym
 import numpy as np
 import random
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
 
 env = gym.make('FrozenLake-v0')
@@ -20,81 +19,69 @@ nextQ = tf.placeholder(shape=[None,4],dtype=tf.float32)
 loss = tf.reduce_sum(tf.square(nextQ - Qout))
 
 
-rate_g = tf.train.exponential_decay(0.001, loss, 1, 0.95)
+rate_g = tf.train.exponential_decay(0.001, loss, 1, 0.99)
 updateModel = tf.train.RMSPropOptimizer(rate_g).minimize(loss)
 
 
 init = tf.initialize_all_variables()
 
+def printQ(sess,env):
+    Qmap = sess.run([Qout],feed_dict={inputs1:np.identity(16)})
+    print("Qmap>>#a=0:L, 1:D , 2:R, 3:U")
+    print(Qmap)
+    env.render()
+
+
 # Set learning parameters
-y = .5
-e = 0.1
+e = 0.5
 num_episodes = 4000
 #create lists to contain total rewards and steps per episode
-jList = []
-rList = []
 ave_r = 0
 with tf.Session() as sess:
     sess.run(init)
-
-
-
     print("env.action_space>>",env.action_space.__dict__)
     for i in range(num_episodes):
         #Reset environment and get first new observation
-        s = env.reset()
-        rAll = 0
-        d = False
+        ns = env.reset()
+        end = False
         j = 0
 
+
+        #Choose an action by greedily (with e chance of random action) from the Q-network
+        [nQ] = sess.run([Qout],feed_dict={inputs1:[np.identity(16)[ns]]})
         #The Q-Network
         while j < 20:
             j+=1
-
-            #Choose an action by greedily (with e chance of random action) from the Q-network
-            inp1=np.identity(16)[s]
-            [allQ] = sess.run([Qout],feed_dict={inputs1:[inp1]})
-
-            a = np.argmax(allQ)
+            cs,cQ,a = ns,nQ,np.argmax(nQ)
+            #print(cs)
 
             if np.random.rand(1) < e:
                 a = env.action_space.sample()
 
-            #print("allQ>>",allQ, " a>>",a, "type(a)>>",type(a))
-            #print("a>>",a)
-            #Get new state and reward from environment
-            s1,r,d,_ = env.step(a)
-            ave_r +=r
-            if( (d==True or j==19) and r!=1 ) :
-                r=-1
-            if (i%1000 == 0) :
-                env.render()
-            #Obtain the Q' values by feeding the new state through our network
-            inp1=np.identity(16)[s1]
-            Q1 = sess.run(Qout,feed_dict={inputs1:[inp1]})
+            
+            ns,nr,end,_ = env.step(a)
+
+
+            if( j == 20 ):
+                end=True#enough
+            if( end==True and nr!=1 ) :
+                nr=-1
+            nQ = sess.run(Qout,feed_dict={inputs1:[np.identity(16)[ns]]})
             #Obtain maxQ' and set our target value for chosen action.
-            maxQ1 = np.max(Q1)
-            #print("allQ>>",allQ)
-            targetQ = allQ
-
-            targetQ[0,a] = r + y*maxQ1
+            cQ[0,a] += 0.5*( nr + 0.5*(np.max(nQ)+np.min(nQ))/2 - cQ[0,a])
             #Train our network using target and predicted Q values
-            _ = sess.run([updateModel],feed_dict={inputs1:np.identity(16)[s:s+1],nextQ:targetQ})
-            rAll += r
-            s = s1
-            if d == True:
-                #Reduce chance of random action as we train the model.
+            _ = sess.run([updateModel],feed_dict={inputs1:[np.identity(16)[cs]],nextQ:cQ})
 
-                e = 1./((i/50) + 10)
+            '''if (i%500 == 0) :
+                env.render()'''
+
+            if end == True:
+                e *= 0.9
+                if (i%500 == 499) :
+                    printQ(sess,env)
                 break
 
+        if nr == 1 :ave_r +=1
         if i%100 == 0 :
-            print ("episodes: ", i, " ave_r:", ave_r/100)
+            print ("episodes: ", i, " ave_r:", ave_r,"%")
             ave_r = 0
-        jList.append(j)
-        rList.append(rAll)
-print ("Percent of succesful episodes: " + str(sum(rList)/num_episodes) + "%")
-
-plt.plot(rList)
-
-plt.plot(jList)
