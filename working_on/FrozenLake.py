f6818141c0fdd60b4052064c430eca0e14ec0813
Chia-Pin Tseng <chiapintseng@gmail.com>
2017-04-22 19:12:09 -0700

    Experience reward balance

--- working_on/FrozenLake.py
+++ working_on/FrozenLake.py
@@ -13,7 +13,7 @@ register(
             }
         )
 
-env = gym.make('FrozenLake-v1')
+env = gym.make('FrozenLake-v0')
 tf.reset_default_graph()
 
 
@@ -22,7 +22,7 @@ def lrelu(x, leak=0.2, name="lrelu"):
          rlx=tf.nn.relu(x);
          rlnx=tf.nn.relu(-x);
          return rlx-leak*rlnx
-def relu(x, leak=0.2, name="relu"):
+def relu(x, name="relu"):
     with tf.variable_scope(name):
         return tf.nn.relu(x)
 
@@ -47,9 +47,9 @@ class NNetwork_Obj:
             input_dim = net_input.shape.as_list()[1];
             output_dim = targetOutput.shape.as_list()[1];
 
-            W = tf.Variable(tf.random_uniform([input_dim,15],-0.01,0.01))
-            B = tf.Variable(tf.random_uniform([15],-0.01,0.01))
-            Wo = tf.Variable(tf.random_uniform([15,output_dim],-0.01,0.01))
+            W = tf.Variable(tf.random_uniform([input_dim,25],-0.01,0.01))
+            B = tf.Variable(tf.random_uniform([25],-0.01,0.01))
+            Wo = tf.Variable(tf.random_uniform([25,output_dim],-0.01,0.01))
             Bo = tf.Variable(tf.random_uniform([output_dim],-0.01,0.01))
 
             H1 = relu(tf.matmul(net_input,W)+B)
@@ -64,12 +64,12 @@ class NNetwork_Obj:
             L2_reg = [p.assign(p*0.999) for p in self.Ws]
             L1_reg = [p.assign(p-0.000001*tf.sign(p)) for p in self.Ws]
 
-            regularizer = tf.contrib.layers.l2_regularizer(scale=0.003, scope=None)
+            regularizer = tf.contrib.layers.l2_regularizer(scale=0.001, scope=None)
             regularization_penalty = tf.contrib.layers.apply_regularization(regularizer, self.Ws)
 
             regularized_loss = loss +regularization_penalty  # this loss needs to be minimized
 
-            self.train = tf.train.RMSPropOptimizer(0.001).minimize(regularized_loss)
+            self.train = tf.train.GradientDescentOptimizer(0.01).minimize(regularized_loss)
 
 
 class QLearning_OBJ:
@@ -79,8 +79,6 @@ class QLearning_OBJ:
     net_obj = None
     graph = None
 
-    exp_set={'cs':[],'a':[],'nr':[],'ns':[]}
-    exp_reward_sum = 0
 
     def __init__(self,graph):
         self.graph = graph
@@ -95,77 +93,58 @@ class QLearning_OBJ:
             return sess.run([self.net_obj.output],feed_dict={self.state_in:states})
 
 
+    exp_set={'cs':[],'a':[],'nr':[],'ns':[]}
     def training_exp_replay_set(self,experience):
         self.training_exp_replay(experience['cs'],experience['a'],experience['nr'],experience['ns'])
-    ccc = 0
     def training_exp_replay(self,cs_arr,a_arr,nr_arr,ns_arr):
         # current state, action, next reward, next state
         # => current state, Q; next state-> next Q(nQ)
         # => target Q = nr + gamma*(max(nQ))
         # => train (current state, target Q)
-        self.ccc+=1
         with self.graph.as_default():
             [cQ] = self.QNet(cs_arr);
             [nQ] = self.QNet(ns_arr);
             for i in range(len(cQ)):
                 #print("cs:",cs_arr[i]," a:",a_arr[i]," nr:",nr_arr[i]," ns:",ns_arr[i],"<<<<")
-                cQ[i,a_arr[i]] = nr_arr[i] + 0.5* (np.max(nQ[i]))
+                cQ[i,a_arr[i]] = nr_arr[i] + 0.2* (np.max(nQ[i]))
 
+            #for iii in range(len(cQ)):print(cQ[iii], "<><><", np.argmax( cs_arr[iii]))
             sess.run([self.net_obj.train],feed_dict={self.state_in:cs_arr,self.targetQ_in:cQ})
 
             if(nr_arr[i] !=0):
-                for i in range(len(nQ)):
-                    for j in range(len(nQ[i])):
-                        nQ[i,j]=0
+                nQ.fill(0)
+
                 sess.run([self.net_obj.train],feed_dict={self.state_in:ns_arr,self.targetQ_in:nQ})
-    '''def training_exp_replay(self,cs_arr,a_arr,nr_arr,ns_arr):
-        # current state, action, next reward, next state
-        # => current state, Q; next state-> next Q(nQ)
-        # => target Q = nr + gamma*(max(nQ))
-        # => train (current state, target Q)
-        self.ccc+=1
-        with self.graph.as_default():
-            [cQ] = self.QNet(cs_arr);
-            [nQ] = self.QNet(ns_arr);
-            for i in range(len(cQ)):
-                #if nr_arr[i]==1:
-                #print("cs:",cs_arr[i]," a:",a_arr[i]," nr:",nr_arr[i]," ns:",ns_arr[i],"<<<<")
-                cQ[i,a_arr[i]] = nr_arr[i] + 0.95* (np.max(nQ[i]))
-                if( cQ[i,a_arr[i]]< -1 ):
-                    cQ[i,a_arr[i]] = -1
-            if self.ccc%100 ==0:
-                for i in range(len(cQ)):
-                    print("cs:",np.argmax( cs_arr[i])," cQ:",cQ[i])
-            sess.run([self.net_obj.train],feed_dict={self.state_in:cs_arr,self.targetQ_in:cQ})
-            sess.run([self.net_obj.train],feed_dict={self.state_in:cs_arr,self.targetQ_in:cQ})'''
 
     def training(self,states,tarQ_arr):
         with self.graph.as_default():
             sess.run([self.net_obj.train],feed_dict={self.state_in:states,self.targetQ_in:tarQ_arr})
 
-
-
+    rewardHist_offset =1
+    rewardHist=[0,0,0]
+    experience_limit=500
     def experience_append(self,cs,a,nr,ns):
-        self.exp_reward_sum += nr
-        if len(self.exp_set['cs']) <500:
+
+        if len(self.exp_set['cs']) <self.experience_limit:
             self.exp_set['cs'].append(cs)
             self.exp_set[ 'a'].append(a)
             self.exp_set['nr'].append(nr)
             self.exp_set['ns'].append(ns)
         else:
-            idx = 0
+            most_r = np.argmax( self.rewardHist) - self.rewardHist_offset#find the exp that we had most
+
+            self.rewardHist[int(most_r+self.rewardHist_offset)]-=1
             idx = np.random.randint(len(self.exp_set[ 'cs']))
-            while self.exp_set[ 'nr'][idx] == 1:
-                if np.random.rand(1) <0.1:
-                    break
+            while self.exp_set[ 'nr'][idx] != most_r:
                 idx = np.random.randint(len(self.exp_set[ 'cs']))
 
-
-            self.exp_reward_sum -=self.exp_set[ 'nr'][idx]
             self.exp_set[ 'cs'][idx] = cs
             self.exp_set[ 'a'][idx] = a
             self.exp_set[ 'nr'][idx] = nr
             self.exp_set[ 'ns'][idx] = ns
+
+
+        self.rewardHist[int(nr+self.rewardHist_offset)]+=1
 
 
 
@@ -225,23 +204,21 @@ with tf.Session(graph=graph1) as sess:
             ns,nr,end,_ = env.step(a)
             ns_vec = np.identity(16)[ns]
 
-            if( j==maxMove or cs==ns) :
+            if( j==maxMove ) :
                 end=True
             if( end==True and nr!=1 ) :
                 nr=-1
             [nQ] =  qobj.QNet([ns_vec])
 
-            if np.random.rand(1) <0.1:
-                qobj.experience_append(cs_vec,a,nr,ns_vec)
+            qobj.experience_append(cs_vec,a,nr,ns_vec)
 
             if end == True:
-                qobj.experience_append(cs_vec,a,nr,ns_vec)
                 if nr == 1 :ave_r +=1
-                qobj.training_exp_replay_set(qobj.exp_set)
                 break
 
         if i%100 == 99 :
+            qobj.training_exp_replay_set(qobj.exp_set)
             e *= 0.9
             printQ(sess,env)
-            print ("episodes: ", i, " ave_r:", ave_r,"% e:",e, " exp_reward_sum:",qobj.exp_reward_sum," len(exp):",len(qobj.exp_set['nr']))
+            print ("episodes: ", i, " ave_r:", ave_r,"% e:",e, "rewardHist:",qobj.rewardHist, " len(exp):",len(qobj.exp_set['nr']))
             ave_r = 0
